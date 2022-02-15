e5e5e2cfbc850fe7259587190fd23f63af417c87
orellachichester@gmail.com <orellachichester@gmail.com>
2022-02-15 12:35:42 -0500

    First commit

---
+++ Computer.java
@@ -0,0 +1,25 @@
+package MorningLab;
+
+//Create a Computer class that has 3 variables.
+// The first variable will have a name.
+// The second variable will be the year of the computer.
+// Last the final variable will be the cost of the computer.
+// All 3 variable will be private fields and must use getters to retrieve the data and setters to set the variables.
+
+public class Computer {
+    private String name;
+    private int year;
+    private int cost;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
